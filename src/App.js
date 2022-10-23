b20cb36dacfcda8230236cc2519eddc552e1e77f
iampembo <103342913+iampembo@users.noreply.github.com>
2022-10-23 14:21:03 +1100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/m3IvNCeIcUVKH6eF/scene.splinecode" />
+  );
+}
